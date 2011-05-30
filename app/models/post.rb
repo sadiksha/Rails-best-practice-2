5f2caae3eaf7a3dfac827f11fecdcbd1c1132000
Sadiksha <sadiksha@sproutify.com>
2011-05-30 09:58:53 +0545

    Used seo friendly urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,12 @@ class Post < ActiveRecord::Base
  has_many :comments
 belongs_to :user
 
-def recent_commenters
-    self.comments.recent(5).collect{|comment| comment.user.login}.map{|commenter| "<li>#{commenter}</li>"}.join("")
+ def to_param
+   "#{id}-{self.title}"
+ end
+
+ def to_s
+   "#{post.title} (#{post.comments.size})"
  end
 
 end
